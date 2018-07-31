02523a30471f87d843179c59f9d41b778b0a50ac
jimmynicks1 <Jamesnickele@gmail.com>
2018-07-30 19:46:09 -0500

    add to git

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheese', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Pineapple', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Mushrooms', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('pepperoni', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('organic', false, CURRENT_TIMESTAMP);
